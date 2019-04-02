40156a78a3e322c9f87c8852af3efc8125dfbf51
Sokol Olena <36013697+Sokolheavy@users.noreply.github.com>
2019-04-02 14:43:56 +0300

    Create search_in_savedproc.R

---
+++ Handly_works_scripts/search_in_savedproc.R
@@ -0,0 +1,16 @@
+SELECT QUOTENAME(name)
+FROM sys.objects
+WHERE type='P' AND OBJECT_DEFINITION([object_id]) LIKE N'%i_vintage%';
+
+SELECT QUOTENAME(name)
+FROM sys.objects
+WHERE type='P' AND OBJECT_DEFINITION([object_id]) LIKE N'%Link_IS_AC%';
+
+SELECT DISTINCT
+       o.name AS Object_Name,
+       o.type_desc
+  FROM sys.sql_modules m
+       INNER JOIN
+       sys.objects o
+         ON m.object_id = o.object_id
+ WHERE m.definition Like '%y_products%';
