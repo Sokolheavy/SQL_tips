416e6fd7de38c918021b2f078fe4961367aeba7a
Sokol Olena <36013697+Sokolheavy@users.noreply.github.com>
2019-05-16 12:01:30 +0300

    Update _%_search.sql

--- Handly_works_scripts/_%_search.sql
+++ Handly_works_scripts/_%_search.sql
@@ -1,2 +1,13 @@
+--search with special symbol
 select * from #table1 where name like '%10!%%' ESCAPE '!'
 select * from #table1 where name like '%10[%]%' 
+
+
+--table in database
+SELECT      c.name  AS 'ColumnName'
+            ,t.name AS 'TableName'
+FROM        sys.columns c
+JOIN        sys.tables  t   ON c.object_id = t.object_id
+WHERE       c.name LIKE '%regi%'
+ORDER BY    TableName
+            ,ColumnName;
