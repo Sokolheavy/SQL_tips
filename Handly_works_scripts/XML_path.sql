3063682097f48e7ef9e8cceae9a83a35d135be86
Sokol Olena <36013697+Sokolheavy@users.noreply.github.com>
2019-07-08 14:56:34 +0300

    Update XML_path.sql

--- Handly_works_scripts/XML_path.sql
+++ Handly_works_scripts/XML_path.sql
@@ -9,3 +9,56 @@ SELECT t1.id ,
 		into #reject_rules
 from #All_rejects_union t1 
  group by t1.id
+ 
+ 
+-----------------------------------------------------------------------------------------------------
+ DECLARE @rows nvarchar(max) = '[A],[B],[C]';
+DECLARE @cols nvarchar(max);
+
+SELECT LTRIM(RTRIM(j.value)) FROM STRING_SPLIT(@rows, ',') j
+
+SET @cols = STUFF((SELECT ', ' + (col) 
+                   FROM (SELECT LTRIM(RTRIM(j.value)) col FROM STRING_SPLIT(@rows, ',') j) Foo
+            FOR XML PATH(''), TYPE
+            ).value('.', 'nvarchar(MAX)') 
+           ,1,2,'');
+
+SELECT @cols;
+
+SET @cols = STUFF((SELECT ', ' + ('SUM(' + col + ')') 
+                   FROM (SELECT LTRIM(RTRIM(j.value)) col FROM STRING_SPLIT(@rows, ',') j) Foo
+            FOR XML PATH(''), TYPE
+            ).value('.', 'nvarchar(MAX)') 
+           ,1,2,'');
+
+SELECT @cols;
+
+SET @cols = STUFF((SELECT ', ' + ('SUM(' + col + ') ' + col) 
+                   FROM (SELECT LTRIM(RTRIM(j.value)) col FROM STRING_SPLIT(@rows, ',') j) Foo
+            FOR XML PATH(''), TYPE
+            ).value('.', 'nvarchar(MAX)') 
+           ,1,2,'');
+
+SELECT @cols;
+GO
+
+		   
+		   
+		   
+| (No column name) |
+| :--------------- |
+| [A]              |
+| [B]              |
+| [C]              |
+
+| (No column name) |
+| :--------------- |
+| [A], [B], [C]    |
+
+| (No column name)             |
+| :--------------------------- |
+| SUM([A]), SUM([B]), SUM([C]) |
+
+| (No column name)                         |
+| :--------------------------------------- |
+| SUM([A]) [A], SUM([B]) [B], SUM([C]) [C] |		   
